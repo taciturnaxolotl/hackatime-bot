e9d7c261069e430b46f0ca68127e24b28d5635d5
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2025-02-24 01:28:00 -0500

    feat: add a screen for already created accounts

--- features/command.ts
+++ features/command.ts
@@ -2,6 +2,49 @@ import { slackApp } from "../index";
 
 const command = async () => {
   slackApp.command("/hackatime", async ({ context }) => {
+    const hackatimeUser: { apiKey: string } | null = await fetch(
+      `https://waka.hackclub.com/api/special/apikey?user=${context.userId}`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.HACKATIME_API_KEY}`,
+        },
+      },
+    ).then((res) => (res.status === 404 ? null : res.json()));
+
+    if (hackatimeUser) {
+      if (context?.respond)
+        await context.respond({
+          response_type: "ephemeral",
+          text: "Hi there! I'm the Hackatime bot :hyper-dino-wave:",
+          blocks: [
+            {
+              type: "section",
+              text: {
+                type: "mrkdwn",
+                text: "Hi there! I'm the Hackatime bot :hyper-dino-wave:",
+              },
+            },
+            {
+              type: "section",
+              text: {
+                type: "mrkdwn",
+                text: `It looks like you already have an account! You can log in to the <https://waka.hackclub.com/login|Hackatime dashboard> with your username \`${context.userId}\` and password :3c:`,
+              },
+            },
+            {
+              type: "context",
+              elements: [
+                {
+                  type: "mrkdwn",
+                  text: `your api key for the Hackatime API is \`${hackatimeUser.apiKey}\``,
+                },
+              ],
+            },
+          ],
+        });
+      return;
+    }
+
     context.respond({
       response_type: "ephemeral",
       text: "Hi there! I'm the Hackatime bot :hyper-dino-wave:",
